7116dc115ec0522219bf74bfc8772f4522cdcf42
unknown <tnwl9540@gmail.com>
2024-05-30 03:19:29 +0900

    0319

--- src/redux/modules/expenses.jsx
+++ src/redux/modules/expenses.jsx
@@ -16,8 +16,23 @@ const expensesSlice = createSlice({
     setExpenses: (state, action) => {
       return action.payload;
     },
+    editedExpense: (state, action) => {
+      const { id, date, item, amount, description } = action.payload;
+      const detailExpense = expenses.find((expense) => expense.id === param.id);
+      if (detailExpense) {
+        detailExpense.date = date;
+        detailExpense.item = item;
+        detailExpense.amount = amount;
+        detailExpense.description = description;
+      }
+    },
+    newExpenses: (state, action) => {
+      const { id } = action.payload;
+      return state.filter((expense) => expense.id !== id);
+    },
   },
 });
 
-export const { setExpenses } = expensesSlice.actions;
+export const { setExpenses, editedExpense, newExpenses } =
+  expensesSlice.actions;
 export default expensesSlice.reducer;
